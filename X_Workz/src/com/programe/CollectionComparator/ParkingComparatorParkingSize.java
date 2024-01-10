5bdaba9bdff6c468fdc8ee4f7d73dd26477ef8fd
NitinMoger <nitin.xworkz@gmail.com>
2024-01-10 23:34:53 +0530

    Ascending and Descending

---
+++ X_Workz/src/com/programe/CollectionComparator/ParkingComparatorParkingSize.java
@@ -0,0 +1,12 @@
+package com.programe.CollectionComparator;
+
+import com.programe.collectionDTO.ParkingDTO;
+
+import java.util.Comparator;
+
+public class ParkingComparatorParkingSize  implements Comparator<ParkingDTO> {
+    @Override
+    public int compare(ParkingDTO o1, ParkingDTO o2) {
+        return Double.compare(o1.getParkingSize(), o2.getParkingSize());
+    }
+}
